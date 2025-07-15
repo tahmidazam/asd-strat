a20a87e76e9921b3b96b83bafbd2a9a8348ea05d
Tahmid Azam <tahmid_azam@icloud.com>
2025-07-15 10:36:37 -0400

    init

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "asd-strat"
+copyright = "2025, Tahmid Azam"
+author = "Tahmid Azam"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+html_theme_options = {
+    "caption_font_family": "system-ui",
+    "font_family": "system-ui",
+    "head_font_family": "system-ui",
+}
